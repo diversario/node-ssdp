61695fa4523427da7e19de8e520e94adab0b9c4c
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2012-10-18 11:40:09 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,65 @@
+## Usage - Client
+
+	```javascript
+	var SSDP = require('ssdp').SSDP;
+	var client = new SSDP;
+	
+	client.on('notify', function () {
+		console.log('Got a notification.');
+	});
+	
+	client.on('response', function inResponse(msg, rinfo) {
+		console.log('Got a response to an m-search.');
+	});
+	
+	ssdp.search('urn:schemas-upnp-org:service:ContentDirectory:1');
+	
+	// Or maybe if you want to scour for everything
+	
+	ssdp.search('ssdp:all');
+	
+	// This should get you at least started.
+
+
+## Usage - Server
+
+	```javascript
+	var SSDP = require('ssdp').SSDP;
+	
+	var server = new SSDP;
+	server.addUSN('upnp:rootdevice');
+	server.addUSN('urn:schemas-upnp-org:device:MediaServer:1');
+	server.addUSN('urn:schemas-upnp-org:service:ContentDirectory:1');
+	server.addUSN('urn:schemas-upnp-org:service:ConnectionManager:1');
+	
+	server.on('advertise-alive', function (heads) {
+		// Expire old devices from your cache.
+		// Register advertising device somewhere (as designated in http headers heads)
+	}
+	
+	server.on('advertise-bye', function (heads) {
+		// Remove specified device from cache.
+	}
+	
+	// This should get your local ip to pass off to the server.
+	require('dns').lookup(require('os').hostname(), function (err, add) {
+		server.server(add);
+	};
+
+
+## Author
+
+Initial commit of this module is a clone of https://bitbucket.org/Xedecimal/node-ssdp, commit 0c6cd0a (2012-03-21).
+
+Forked with author's permission.
+
+# License
+
+(The MIT License)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ ssdp.js
@@ -0,0 +1,199 @@
+var dgram = require('dgram');
+var execFile = require('child_process').execFile;
+
+var events = require('events');
+var util = require('util');
+
+SSDP = function SSDP() {
+	if (!(this instanceof SSDP)) return new SSDP();
+
+	events.EventEmitter.call(this);
+
+	var SSDP_SIG = 'Microsoft-Windows-NT/5.1 UPnP/1.1 Mediabrary/1.0';
+	var SSDP_IP = '239.255.255.250';
+	var SSDP_PORT = 1900;
+	var SSDP_IPPORT = SSDP_IP+':'+SSDP_PORT;
+	var TTL = 1800;
+
+	this.usns = {};
+	this.udn = 'uuid:e3f28962-f694-471f-8f74-c6abd507594b';
+
+	// Configure socket for either client or server.
+	this.sock = dgram.createSocket('udp4');
+	this.sock.on('error', function () {
+		console.log('############### Got an error!');
+	});
+
+	this.sock.on('message', function onMessage(msg, rinfo) {
+		this.parseMessage(msg, rinfo);
+	}.bind(this));
+
+	this.sock.on('listening', function onListening() {
+		var addr = this.sock.address();
+		console.log('SSDP Listening on '+addr.address+':'+addr.port);
+		this.sock.addMembership(SSDP_IP);
+		this.sock.setMulticastTTL(2);
+	}.bind(this));
+
+	this.inMSearch = function (st, rinfo) {
+		var peer = rinfo['address'];
+		var port = rinfo['port'];
+
+		if (st[0] == '"') st = st.slice(1, -1);
+
+		for (usn in this.usns) {
+			udn = this.usns[usn];
+
+			if (st == 'ssdp:all' || usn == st) {
+				var pkt = this.getSSDPHeader('200 OK', {
+					ST: usn,
+					USN: udn,
+					LOCATION: this.httphost+'/upnp/desc.php',
+					'CACHE-CONTROL': 'max-age='+TTL,
+					DATE: new Date().toUTCString(),
+					SERVER: SSDP_SIG,
+					EXT: ''
+				}, true);
+				console.log('Sending a 200 OK for an m-search to '+peer+':'+port);
+				pkt = new Buffer(pkt);
+				this.sock.send(pkt, 0, pkt.length, port, peer);
+			}
+		}
+	}
+
+	process.on('exit', function () { this.close; } );
+
+	this.addUSN = function (device) {
+		this.usns[device] = this.udn+'::'+device;
+	}
+
+	this.parseMessage = function (msg, rinfo) {
+		var type = msg.toString().split('\r\n').shift();
+
+		// HTTP/#.# ### Response
+		if (type.match(/HTTP\/(\d{1})\.(\d{1}) (\d+) (.*)/))
+			this.parseResponse(msg, rinfo);
+		else
+			this.parseCommand(msg, rinfo);
+	}
+
+	this.parseCommand = function parseCommand(msg, rinfo) {
+		var lines = msg.toString().split("\r\n");
+		var type = lines.shift().split(' ');
+		var method = type[0];
+		var uri = type[1];
+
+		var heads = {};
+		for (ix in lines) {
+			var line = lines[ix];
+			if (line.length < 1) continue;
+			var vv = line.match(/^([^:]+):\s*(.*)$/);
+			heads[vv[1].toUpperCase()] = vv[2];
+		}
+
+		switch (method) {
+			case 'NOTIFY':
+				// Device coming to life.
+				if (heads['NTS'] == 'ssdp:alive') {
+					this.emit('advertise-alive', heads);
+				}
+				// Device shutting down.
+				else if (heads['NTS'] == 'ssdp:byebye') {
+					this.emit('advertise-bye', heads);
+				}
+				else console.log('############### Notify unhandled!');
+				break;
+			case 'M-SEARCH':
+				console.log('SSDP M-SEARCH: for ('+heads['ST']+') from ('+rinfo['address']+':'+rinfo['port']+')');
+				if (!heads['MAN']) return;
+				if (!heads['MX']) return;
+				if (!heads['ST']) return;
+				this.inMSearch(heads['ST'], rinfo);
+				break;
+			default:
+				console.log("Unknown message: \r\n"+msg);
+		}
+	}
+	
+	this.parseResponse = function parseResponse(msg, rinfo) {
+		this.emit('response', msg, rinfo);
+		/*console.log('Parsing a response!');
+		console.log(msg.toString());*/
+	}
+
+	this.search = function search(st) {
+		require('dns').lookup(require('os').hostname(), function (err, add) {
+			this.sock.bind(0, add);
+
+			var pkt = this.getSSDPHeader('M-SEARCH', {
+				HOST: SSDP_IPPORT,
+				ST: st,
+				MAN: '"ssdp:discover"',
+				MX: 3
+			});
+			pkt = new Buffer(pkt);
+			console.log(pkt.toString());
+			this.sock.send(pkt, 0, pkt.length, SSDP_PORT, SSDP_IP);
+		}.bind(this));
+	}
+
+	this.server = function (ip) {
+		this.httphost = 'http://'+ip+':10293';
+
+		this.usns[this.udn] = this.udn;
+
+		this.sock.bind(SSDP_PORT, ip);
+
+		// Shut down.
+		this.advertise(false);
+		setTimeout(this.advertise.bind(this), 1000, false);
+
+		// Wake up.
+		setTimeout(this.advertise.bind(this), 2000);
+		setTimeout(this.advertise.bind(this), 3000);
+		
+		// Ad loop.
+		setInterval(this.advertise.bind(this), 10000);
+	}
+
+	this.close = function () {
+		this.advertise(false);
+		this.advertise(false);
+		this.sock.close();
+	}
+
+	this.advertise = function (alive) {
+		if (!this.sock) return;
+		if (alive == undefined) alive = true;
+
+		for (usn in this.usns) {
+			udn = this.usns[usn];
+
+			var out = 'NOTIFY * HTTP/1.1\r\n';
+			heads = {
+				HOST: SSDP_IPPORT,
+				NT: usn,
+				NTS: (alive ? 'ssdp:alive' : 'ssdp:byebye'),
+				USN: udn
+			}
+			if (alive) {
+				heads['LOCATION'] = this.httphost+'/upnp/desc.php';
+				heads['CACHE-CONTROL'] = 'max-age=1800';
+				heads['SERVER'] = SSDP_SIG;
+			}
+			out = new Buffer(this.getSSDPHeader('NOTIFY', heads));
+			this.sock.send(out, 0, out.length, SSDP_PORT, SSDP_IP);
+		}
+	}
+
+	this.getSSDPHeader = function (head, vars, res) {
+		if (res == undefined) res = false;
+		if (res) ret = "HTTP/1.1 "+head+"\r\n";
+		else ret = head+" * HTTP/1.1\r\n";
+		for (n in vars) ret += n+": "+vars[n]+"\r\n";
+		return ret+"\r\n";
+	}
+}
+
+util.inherits(SSDP, events.EventEmitter);
+exports.SSDP = SSDP;
