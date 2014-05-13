b22e783ed15754d396a545a428021560b7ffe68c
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2014-05-12 21:23:57 -0700

    Separate client and server.
    Re: #12.

--- test/client.js
+++ test/client.js
@@ -1,16 +1,17 @@
-var ssdp = require('../')
-  , client = new ssdp({logLevel: 'TRACE', log: true});
+var ssdp = require('../').Client
+  , client = new ssdp({logLevel: 'TRACE', log: true})
 
 client.on('notify', function () {
-  //console.log('Got a notification.');
-});
+  //console.log('Got a notification.')
+})
 
 client.on('response', function inResponse(msg, rinfo) {
-  console.log('Got a response to an m-search:', msg.toString());
-});
+  console.log('Got a response to an m-search:', msg.toString())
+})
 
-client.search('urn:schemas-upnp-org:service:ContentDirectory:1');
+client.search('urn:schemas-upnp-org:service:ContentDirectory:1')
 
 // Or maybe if you want to scour for everything
-
-client.search('ssdp:all');
+setInterval(function() {
+  client.search('ssdp:all')
+}, 10000)
