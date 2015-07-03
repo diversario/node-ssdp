21b4bcbb67b660d7d7f581a9c9c534a9f0777264
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2015-07-03 07:29:32 -0700

    Define path first.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var path = process.env.SSDP_COV ? './lib-cov/' : './lib/'
+
 module.exports = {
-  Server: process.env.SSDP_COV ? require('./lib-cov/server') : require('./lib/server'),
-  Client: process.env.SSDP_COV ? require('./lib-cov/client') : require('./lib/client'),
-  Base: process.env.SSDP_COV ? require('./lib-cov/index') : require('./lib/index')
+  Server: require(path + 'server'),
+  Client: require(path + 'client'),
+  Base: require(path + 'index')
 }
